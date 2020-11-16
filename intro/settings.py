efab702e0127410ecf9f35ed998e19cf8a63abda
Tran Thu <56596865+TranThu2970@users.noreply.github.com>
2020-11-16 22:28:23 +0700

    Update settings.py

--- intro/settings.py
+++ intro/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/introCom/images')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static-root')
+MEDIA_URL = '/images/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'static/introCom/images')
+MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles','media-root')
