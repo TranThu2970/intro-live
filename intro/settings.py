0ae091ef886440817742e9a392f7a26b0203b711
TRAN THU <ak2970.mta@gmail.com>
2020-11-16 23:25:52 +0700

    add staticfiles

--- intro/settings.py
+++ intro/settings.py
@@ -120,13 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static-root')
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/introCom/images')
-
+#MEDIA_ROOT = os.path.join(BASE_DIR,'static/introCom/images')
+MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles','media-root')
