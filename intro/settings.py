e70164be7031fc31be64859128a8e1e9d7494bc1
Tran Thu <56596865+TranThu2970@users.noreply.github.com>
2020-11-17 14:58:12 +0700

    Update settings.py

--- intro/settings.py
+++ intro/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
